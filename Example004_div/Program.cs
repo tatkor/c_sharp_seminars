12eaf71cda9abacc37b0cdc38d4ee06db67437a6
tatkor <tatkor@mail.ru>
2023-03-11 18:59:11 +0300

    add Example004_div

---
+++ Example004_div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 10);
+double numberB = new Random().Next(1, 10);
+double result = numberA / numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);
