45943a06a46bc1114658266ce74a88a887b01864
tatkor <tatkor@mail.ru>
2023-03-11 23:22:32 +0300

    add Example005_If_Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿string user = Console.ReadLine();
+
+if (user == "Маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(user);
+}
